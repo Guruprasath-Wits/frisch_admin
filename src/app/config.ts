e5bb3cc17d5e77f9153d608f2ae238e1fb62a331
Natesh kumar <nateshkumar1406@gmail.com>
2026-03-17 00:40:12 +0530

    Latest Code

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-// export const url = 'https://api.frischfuersie.de';
-// export const file_url = 'https://api.frischfuersie.de';
-export const url = 'http://localhost:4001';
-export const file_url = 'http://localhost:4001';
+export const url = 'https://api.frischfuersie.de';
+export const file_url = 'https://api.frischfuersie.de';
+// export const url = 'http://localhost:4001';
+// export const file_url = 'http://localhost:4001';
